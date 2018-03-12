819a4540cc8e36688643d65856688a64f6a8b9a9
nicolas delcombel <36881822+r0rchard@users.noreply.github.com>
2018-03-12 09:18:11 +0100

    Add files via upload
    pages nico

--- projetweb/campaign_added.php
+++ projetweb/campaign_added.php
@@ -11,6 +11,12 @@ $exp=str_replace("'", "\'", $_POST['experimenter']);
 
 require("connect.php");
 $BDD -> exec( "INSERT INTO campaign(camp_title, camp_description, camp_experimenter) VALUES('$title', '$desc', '$exp')" );
+$BDD -> exec( "CREATE DATABASE  $title (
+                                        usr_id int(11) primary key ,
+                                        reponses int(11) NULL,
+                                        FOREIGN KEY (usr_id) REFERENCES user(usr_id),
+                                        FOREIGN KEY (question_id) REFERENCES user(usr_id)
+                                        ");
 
 include("footer.php");
 ?>
